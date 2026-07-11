01a65cc96b63e586924c91614851a1a9d00d3169
Vinayak Daharwal <vinayakdaharwal@gmail.com>
2026-07-11 18:13:26 +0530

    SmallestElement.java

---
+++ src/Inheritence/SmallestElement.java
@@ -0,0 +1,25 @@
+package Inheritence;
+
+import java.util.Arrays;
+
+public class SmallestElement {
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {5, 3, 6, 1, 7, 4, 7};
+        int n = arr.length;
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n ; i++) {
+
+            if (arr[i] != arr[i+1]) {
+                System.out.println("Smallest Element: " + arr[i]);
+                return; // main method ko terminate kar dega
+            }
+        }
+
+        System.out.println(-1);
+    }
+}
